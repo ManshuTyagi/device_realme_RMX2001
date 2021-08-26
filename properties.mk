e11f6b93c3e16177f5e2da7d25eb4baa0bff51d2
KenHV <yo@kenharris.xyz>
2021-08-26 11:16:16 +0530

    RMX2001: Introduce memory management tweaks
    Change-Id: I2446072572d8e801818bd190c448e0cb10b6517f
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- properties.mk
+++ properties.mk
@@ -57,6 +57,18 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.corvus.maintainer=ManshuTyagi
 
+# Memory Management
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \ 
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+
 # NFC
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.nfc.disPowerSave=1 \
